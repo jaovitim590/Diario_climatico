b1776a3b94a67ae163b74d1b2db88da0e7e2831e
jaovitim <joaorodriguesjv590@gmail.com>
2025-06-14 12:28:32 -0300

    finalizando backend

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ fetch('http://localhost:5000/clima')
     const dropdown = document.getElementById('dataDropdown');
     const info = document.getElementById('infoClima');
 
-    // Preenche as opções com os dados
+
     clima.forEach(dia => {
       const option = document.createElement('option');
       option.value = dia.data;
@@ -12,7 +12,6 @@ fetch('http://localhost:5000/clima')
       dropdown.appendChild(option);
     });
 
-    // Escuta mudanças na seleção
     dropdown.addEventListener('change', () => {
       const valorSelecionado = dropdown.value;
 
@@ -30,9 +29,8 @@ fetch('http://localhost:5000/clima')
         return;
       }
 
-      // Exibe os dados formatados horizontalmente
       info.innerHTML = `
-        <div style="display: flex; gap: 40px; align-items: center; flex-wrap: wrap;">
+        <div style="display: flex; gap: 40px; align-items: center; flex-wrap: wrap; margin-bottom: 20px;">
           <div style="text-align: center;">
             <i class="bi bi-thermometer-snow" style="font-size: 40px;"></i>
             <p>${selecionado.temp_min_C}°C</p>
@@ -50,7 +48,13 @@ fetch('http://localhost:5000/clima')
             <p>${selecionado.umidade_media_percent}%</p>
           </div>
         </div>
+
+        <div style="margin-top: 10px;">
+          <p><strong>Nota climática:</strong> ${selecionado.texte}</p>
+          <p><strong>Explicação:</strong> ${selecionado.analise}</p>
+        </div>
       `;
+
       info.style.display = 'block';
     });
   })
